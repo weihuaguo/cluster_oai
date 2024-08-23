656c299c32b8ded1f1bf54c2235e5fbcdb427a8a
weihuaguo <gwh120104@gmail.com>
2024-08-22 23:15:04 -0700

    Add total knee replacement number count

--- cluster_outcome_visualizatin.R
+++ cluster_outcome_visualizatin.R
@@ -72,12 +72,12 @@ kr_df$right_round_year <- str_c("RIGHT_TKR_Y", str_pad(round(kr_df$right_time/36
 lkr_spread <- spread(kr_df[,c("ID", "left_round_year", "left_kr")], "left_round_year", "left_kr")
 rkr_spread <- spread(kr_df[,c("ID", "right_round_year", "right_kr")], "right_round_year", "right_kr")
 kr_spread <- merge(lkr_spread, rkr_spread, by = "ID")
-kr_spread[is.na(kr_spread)] <- "3: No"
+kr_spread[is.na(kr_spread)] <- "4: Unattended/Dropped-out"
 kr_gath <- gather(kr_spread, "time_side", "kr", colnames(kr_spread)[str_detect(colnames(kr_spread), "TKR_Y")])
 kr_gath$side <- str_split_fixed(kr_gath$time_side, "_TKR_", n = 2)[,1]
 kr_gath$time <- str_split_fixed(kr_gath$time_side, "_TKR_", n = 2)[,2]
 kr_gath$tkr <- kr_gath$kr
-kr_gath$tkr[str_detect(kr_gath$tkr, "\\.\\:\\ Missing")] <- "3: No"
+kr_gath$tkr[str_detect(kr_gath$tkr, "\\.\\:\\ Missing")] <- "3: No/Missing"
 
 kr_umap_df <- merge(kr_gath, umap_df, by = "ID", all.x = T)
 
@@ -92,7 +92,40 @@ bar_gg <- ggplot(kr_sum_df, aes(x = time, y = n, fill = tkr)) +
 	labs(x = "Time (year)", y = "Relative number of knees", fill = "Total knee replacement") +
 	theme_bw() +
 	theme(legend.position = "top")
+ggsave(paste(ppf, "tkr_bar_fill.png", sep = ""), dpi = png_res, width = 16, height = 6)
+
+bar_gg <- ggplot(kr_sum_df, aes(x = time, y = n, fill = tkr)) +
+	geom_bar(position = "stack", stat = "identity") +
+	scale_fill_brewer(palette = "Set1") +
+	facet_grid(side~Cluster) +
+	labs(x = "Time (year)", y = "Knee numbers", fill = "Total knee replacement") +
+	theme_bw() +
+	theme(legend.position = "top")
 ggsave(paste(ppf, "tkr_bar_stack.png", sep = ""), dpi = png_res, width = 16, height = 6)
+
+kr_sum_df <- kr_umap_df %>%
+	filter(!is.na(Cluster)) %>%
+	group_by(side, time, tkr) %>%
+	summarize(n = n())
+bar_gg <- ggplot(kr_sum_df, aes(x = time, y = n, fill = tkr)) +
+	geom_bar(position = "fill", stat = "identity") +
+	scale_fill_brewer(palette = "Set1") +
+	facet_grid(side~.) +
+	labs(x = "Time (year)", y = "Relative number of knees", fill = "Total knee replacement") +
+	guides(fill = guide_legend(nrow=2,byrow=TRUE)) +
+	theme_bw() +
+	theme(legend.position = "top")
+ggsave(paste(ppf, "tkr_bar_fill_together.png", sep = ""), dpi = png_res, width = 6, height = 6)
+
+bar_gg <- ggplot(kr_sum_df, aes(x = time, y = n, fill = tkr)) +
+	geom_bar(position = "stack", stat = "identity") +
+	scale_fill_brewer(palette = "Set1") +
+	facet_grid(side~.) +
+	labs(x = "Time (year)", y = "Knee numbers", fill = "Total knee replacement") +
+	guides(fill = guide_legend(nrow=2,byrow=TRUE)) +
+	theme_bw() +
+	theme(legend.position = "top")
+ggsave(paste(ppf, "tkr_bar_stack_together.png", sep = ""), dpi = png_res, width = 6, height = 6)
 
 outcome_patterns<-c('XRKLL', 'XRKLR','WOMADLL', 'WOMADLR', 'WOMKPL', 'WOMKPR', 'MCMJSWL', 'MCMJSWR', 
 		    'WOMSTFL', 'WOMSTFR', 'WOMTSL', 'WOMTSR')
